7e4758ee1674552f50227413c661fdfe10404d77
りき <52626675+riki02@users.noreply.github.com>
2019-11-03 21:16:18 +0900

    Create member_add.php

---
+++ member_add.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href=".css" rel="stylesheet" type="text/css" media="all">
+<title>一言掲示板</title>
+</head>
+<body>
+
+会員追加<br />
+<br />
+<form method="post" action="member_add_check.php">
+会員名を入力してください。<br />
+<input type="text"name="name" style="width:200px"><br />
+パスワードを入力してください。<br />
+<input type="password"name="pass" style="width:100px"><br />
+パスワードをもう1度入力してください。<br />
+<input type="password" name="pass2" style="width100px"><br />
+<br /> 
+<input type="button"onclick="history.back()"value="戻る">
+<input type="submit"value="ＯＫ">
+</form>
+
+</body>
+</html>
